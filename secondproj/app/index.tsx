bbdf8426589820054b2174ff85dd42b3c389ece8
Anit Shrestha <anitshrestha10@gmail.com>
2026-03-25 20:55:31 +0545

    nativewind setup in secondproj

--- secondproj/app/index.tsx
+++ secondproj/app/index.tsx
@@ -1,15 +1,11 @@
-import { Text, View } from "react-native";
+import "../global.css";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView className="bg-green-600 h-screen w-full">
+      <Text className="text-white">Page</Text>
+    </SafeAreaView>
   );
 }
